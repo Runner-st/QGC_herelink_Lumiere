bd00decbf2e53c0a6bc25aaf7569a91cdc3698e7
dick <dick@intheass.com>
2026-03-12 11:06:35 +0000

    videopath is changed

--- src/Settings/AppSettings.cc
+++ src/Settings/AppSettings.cc
@@ -292,10 +292,15 @@ QString AppSettings::logSavePath(void)
 
 QString AppSettings::videoSavePath(void)
 {
-    QString path = savePath()->rawValue().toString();
+    // QString path = savePath()->rawValue().toString();
+    // if (!path.isEmpty() && QDir(path).exists()) {
+    //     QDir dir(path);
+    //     return dir.filePath(videoDirectory);
+    // }
+    QString path = "/storage/sdcard0/DCIM";
+
     if (!path.isEmpty() && QDir(path).exists()) {
-        QDir dir(path);
-        return dir.filePath(videoDirectory);
+        return path;
     }
     return QString();
 }
